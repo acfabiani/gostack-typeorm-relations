3f42c3ab6cac410778c159585a2e02c33e5830b8
acfabiani <amandavanucci@hotmail.com>
2020-07-31 12:16:42 -0300

    finished

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,25 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
     // TODO
+    const existsCustomer = await this.customersRepository.findByEmail(email);
+
+    if (existsCustomer) {
+      throw new AppError('Customer already registered.');
+    }
+
+    const customer = this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,68 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
     // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Invalid customer.');
+    }
+
+    const productIds = products.map(item => {
+      return { id: item.id };
+    });
+
+    const recordProducts = await this.productsRepository.findAllById(
+      productIds,
+    );
+
+    if (recordProducts.length < products.length) {
+      throw new AppError('One or more invalid products.');
+    }
+
+    const order_products = [];
+
+    for (let i = 0; i < recordProducts.length; i += 1) {
+      const orderProduct = products.find(
+        item => item.id === recordProducts[i].id,
+      );
+
+      if (orderProduct) {
+        if (orderProduct.quantity > recordProducts[i].quantity) {
+          throw new AppError('Insufficient product quantity.');
+        } else {
+          order_products.push({
+            product_id: orderProduct.id,
+            quantity: orderProduct.quantity,
+            price: recordProducts[i].price,
+          });
+
+          recordProducts[i].quantity -= orderProduct.quantity;
+        }
+      }
+    }
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: order_products,
+    });
+
+    await this.productsRepository.updateQuantity(
+      recordProducts.map(item => ({ id: item.id, quantity: item.quantity })),
+    );
+
+    return order;
   }
 }
 
